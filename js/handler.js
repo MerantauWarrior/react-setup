8541bad4aef7f529c3e24daf951a8702df7a0922
Boris <boris1488ss@gmail.com>
2020-02-18 23:23:02 +0200

    mobile close on fader and menu/category switching

--- js/handler.js
+++ js/handler.js
@@ -5,11 +5,30 @@ document.addEventListener("DOMContentLoaded", function () {
       this.closest('.footer-menu__item').classList.toggle('footer-menu__item_opened');
     })
   });
+  //Close menu/category on fader click
+  document.getElementsByClassName('fader')[0].addEventListener('click', function (event) {
+    if (event.target === document.getElementsByClassName('fader')[0]) {
+      if(window.getComputedStyle(document.getElementsByClassName('menu')[0]).display !== 'none'){
+        document.getElementsByClassName('menu')[0].classList.remove('menu_opened');
+      }
+      if(window.getComputedStyle(document.getElementsByClassName('header-top-menu')[0]).display !== 'none'){
+        document.getElementsByClassName('header-top-menu')[0].classList.remove('header-top-menu_opened');
+      }
+      document.getElementsByClassName('fader')[0].classList.remove('fader_active');
+      document.getElementsByTagName('body')[0].classList.remove('ovh');
+    }
+  });
+
   //Toggle header mobile menu
   document.getElementsByClassName('header-top-menu__mobile')[0].addEventListener('click', function () {
     this.closest('.header-top-menu').classList.toggle('header-top-menu_opened');
     document.getElementsByClassName('fader')[0].classList.toggle('fader_active');
     document.getElementsByTagName('body')[0].classList.toggle('ovh');
+    if(window.getComputedStyle(document.getElementsByClassName('menu')[0]).display !== 'none'){
+      document.getElementsByClassName('menu')[0].classList.remove('menu_opened');
+      document.getElementsByClassName('fader')[0].classList.add('fader_active');
+      document.getElementsByTagName('body')[0].classList.add('ovh');
+    }
   });
   //Toggle catalog mobile menu
   document.getElementsByClassName('header-catalog-menu')[0].addEventListener('click', function (e) {
@@ -20,26 +39,19 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 //Select
   let x, i, j, selElmnt, a, b, c;
-  /* Look for any elements with the class "custom-select": */
   x = document.getElementsByClassName("custom-select");
   for (i = 0; i < x.length; i++) {
     selElmnt = x[i].getElementsByTagName("select")[0];
-    /* For each element, create a new DIV that will act as the selected item: */
     a = document.createElement("DIV");
     a.setAttribute("class", "select-selected");
     a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
     x[i].appendChild(a);
-    /* For each element, create a new DIV that will contain the option list: */
     b = document.createElement("DIV");
     b.setAttribute("class", "select-items select-hide");
     for (j = 1; j < selElmnt.length; j++) {
-      /* For each option in the original select element,
-      create a new DIV that will act as an option item: */
       c = document.createElement("DIV");
       c.innerHTML = selElmnt.options[j].innerHTML;
       c.addEventListener("click", function(e) {
-        /* When an item is clicked, update the original select box,
-        and the selected item: */
         let y, i, k, s, h,f;
         s = this.parentNode.parentNode.getElementsByTagName("select")[0];
         f = this.parentNode.parentNode.getElementsByTagName("form")[0];
@@ -62,8 +74,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     x[i].appendChild(b);
     a.addEventListener("click", function(e) {
-      /* When the select box is clicked, close any other select boxes,
-      and open/close the current select box: */
       e.stopPropagation();
       closeAllSelect(this);
       this.nextSibling.classList.toggle("select-hide");
@@ -71,8 +81,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
   function closeAllSelect(elmnt) {
-    /* A function that will close all select boxes in the document,
-    except the current select box: */
     let x, y, i, arrNo = [];
     x = document.getElementsByClassName("select-items");
     y = document.getElementsByClassName("select-selected");
@@ -89,8 +97,6 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
   }
-  /* If the user clicks anywhere outside the select box,
-  then close all select boxes: */
   document.addEventListener("click", closeAllSelect);
 
 
